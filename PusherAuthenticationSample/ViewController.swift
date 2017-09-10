48fa3259747093d155bb58d4ba01231dad88c839
adyrda2 <adyrda2@gmail.com>
2017-09-10 15:45:50 -0500

    Initial Commit

---
+++ PusherAuthenticationSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PusherAuthenticationSample
+//
+//  Created by Angela Dyrda on 9/10/17.
+//  Copyright © 2017 Angela Dyrda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
